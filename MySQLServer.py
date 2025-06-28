ffa868dfef7c9b0ef9e630fb7ddb2268aa76b047
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-06-28 01:30:18 -0700

    connect to database

---
+++ MySQLServer.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from mysql.connector import Error
+try: 
+    my_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Maryam@2112",
+        port="3307"
+    )
+    my_cursor = my_db.cursor()
+    my_cursor.execute("""CREATE DATABASE IF NOT EXISTS alx_book_store""")
+    print("Database 'alx_book_store' created successfully!")
+except Error as e:
+    print(f"Error to create database {e}");
+    
+    
