9e0f73ec9a2493f1fc489fe168973fbe919e10e3
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-06-28 02:18:16 -0700

    show table description using information schema

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,9 @@
 use alx_book_store
 
-SHOW CREATE TABLE  authors
-SHOW CREATE TABLE  books
-SHOW CREATE TABLE customers
-SHOW CREATE TABLE order_details
-SHOW CREATE TABLE orders
+
+INFORMATION_SCHEMA.COLUMNS ", "SELECT", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'
+-- SHOW CREATE TABLE  authors
+-- SHOW CREATE TABLE  books
+-- SHOW CREATE TABLE customers
+-- SHOW CREATE TABLE order_details
+-- SHOW CREATE TABLE orders
